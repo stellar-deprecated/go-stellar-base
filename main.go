a731f2a3465076693c8c425ca58361d3b15bf42b
Scott Fleckenstein <nullstyle@gmail.com>
2015-03-28 17:33:27 -0700

    Remove generated files... we're not ready yet

--- main.go
+++ main.go
@@ -1,5 +1,1 @@
 package stellarcore
-
-//go:generate bundle exec xdrgen -l go -o . -n stellarcore xdr/Stellar-types.x
-//go:generate bundle exec xdrgen -l go -o . -n stellarcore xdr/Stellar-ledger-entries.x
-//go:generate go fmt
