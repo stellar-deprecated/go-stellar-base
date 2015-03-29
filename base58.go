7c5d4ee6f37d561413794a1cf429345e84784ea5
Scott Fleckenstein <nullstyle@gmail.com>
2015-03-29 00:08:10 -0700

    Finish base58 encoding

--- base58.go
+++ base58.go
@@ -1,8 +1,10 @@
 package stellarcore
 
 import (
+	"fmt"
 	"math/big"
 	"strconv"
+	"strings"
 )
 
 const alphabet = "gsphnaf39wBUDNEGHJKLM4PQRST7VWXYZ2bcdeCr65jkm8oFqi1tuvAxyz"
@@ -24,30 +26,77 @@ func (e CorruptInputError) Error() string {
 	return "illegal base58 data at input byte " + strconv.FormatInt(int64(e), 10)
 }
 
+type InvalidVersionByteError struct {
+	Expected VersionByte
+	Actual   VersionByte
+}
+
+func (e InvalidVersionByteError) Error() string {
+	return fmt.Sprintf("illegal base58 version byte expected:%d actual:%d", e.Expected, e.Actual)
+}
+
 type VersionByte byte
 
 const (
-	VersionByteAccountID VersionByte = 0x00
-	VersionByteNone                  = 0x01
-	VersionByteSeed                  = 0x33
+	VersionByteAccountID VersionByte = 0
+	VersionByteNone                  = 1
+	VersionByteSeed                  = 33
 )
 
 func EncodeBase58(src []byte) string {
 	bigInt := new(big.Int)
 	bigInt.SetBytes(src)
+	leadingZeroes := strings.Repeat("g", leadingZeroCount(src))
+
 	var resultSlice []byte = make([]byte, 0, 256)
-	resultSlice = EncodeBigToBase58(resultSlice, bigInt)
-	return string(resultSlice)
+	var results = []string{
+		leadingZeroes,
+		string(EncodeBigToBase58(resultSlice, bigInt)),
+	}
+
+	return strings.Join(results, "")
+}
+
+func EncodeBase58Check(version VersionByte, src []byte) string {
+	start := []byte{}
+	withVersion := append(start, byte(version))
+	withPayload := append(withVersion, src...)
+	withChecksum := append(withPayload, base58CheckSum(withPayload)...)
+	// _ = withChecksum
+	// return fmt.Sprintf("b: %s\n", withChecksum)
+	return string(EncodeBase58(withChecksum))
 }
 
 func DecodeBase58(src string) ([]byte, error) {
+	leadingGs := make([]byte, leadingGCount(src))
+
 	bigInt, err := DecodeBase58ToBig([]byte(src))
 
 	if err != nil {
 		return []byte{}, err
 	}
 
-	return bigInt.Bytes(), nil
+	return append(leadingGs, bigInt.Bytes()...), nil
+}
+
+func DecodeBase58Check(version VersionByte, src string) ([]byte, error) {
+	decoded, err := DecodeBase58(src)
+
+	if err != nil {
+		return []byte{}, err
+	}
+
+	decodedVersion := VersionByte(decoded[0])
+	payload := decoded[1 : len(decoded)-4]
+	checksum := decoded[len(decoded)-4:]
+
+	if decodedVersion != version {
+		return []byte{}, InvalidVersionByteError{version, decodedVersion}
+	}
+
+	_ = checksum
+
+	return payload, nil
 }
 
 // Decode a big integer from the bytes. Returns an error on corrupt
@@ -86,4 +135,30 @@ func EncodeBigToBase58(dst []byte, src *big.Int) []byte {
 		dst[i], dst[j] = dst[j], dst[i]
 	}
 	return dst
+}
+
+func base58CheckSum(message []byte) []byte {
+	inner := Hash(message)
+	outer := Hash(inner[:])
+	return outer[0:4]
+}
+
+func leadingZeroCount(src []byte) (result int) {
+	for _, val := range src {
+		if val != 0x00 {
+			return
+		}
+		result++
+	}
+	return
+}
+
+func leadingGCount(src string) (result int) {
+	for _, val := range src {
+		if val != 'g' {
+			return
+		}
+		result++
+	}
+	return
 }
--- base58_test.go
+++ base58_test.go
@@ -5,10 +5,12 @@ import (
 	"testing"
 )
 
-func TestBase58Encoding(t *testing.T) {
+func TestBase58(t *testing.T) {
 	Convey("Given a message", t, func() {
 		unencoded := []byte("hello world")
 		encoded := "StVgDLaUATiyKyV"
+		accountIdEncoded := "gpvQBfBaMiGQZ2xUqW9KNR"
+		seedEncoded := "n3GdokGwy1qJ11qLmsTzoL"
 
 		Convey("When encoding the message", func() {
 			actual := EncodeBase58(unencoded)
@@ -18,12 +20,58 @@ func TestBase58Encoding(t *testing.T) {
 			})
 		})
 
-		Convey("When decoding the message", func() {
+		Convey("When encoding the message as VersionByteAccountID", func() {
+			actual := EncodeBase58Check(VersionByteAccountID, unencoded)
+
+			Convey("The encoded should be correct", func() {
+				So(actual, ShouldEqual, accountIdEncoded)
+			})
+		})
+
+		Convey("When encoding the message as VersionByteSeed", func() {
+			actual := EncodeBase58Check(VersionByteSeed, unencoded)
+
+			Convey("The encoded should be correct", func() {
+				So(actual, ShouldEqual, seedEncoded)
+			})
+		})
+
+		Convey("When decoding the base58 encoded form", func() {
 			actual, err := DecodeBase58(encoded)
 			So(err, ShouldBeNil)
 
-			Convey("The dedoding should be correct", func() {
+			Convey("The decoding should be correct", func() {
 				So(actual, ShouldResemble, unencoded)
+			})
+		})
+
+		Convey("When decoding the base58check AccountID encoded form", func() {
+			actual, err := DecodeBase58Check(VersionByteAccountID, accountIdEncoded)
+			So(err, ShouldBeNil)
+
+			Convey("The decoding should be correct", func() {
+				So(actual, ShouldResemble, unencoded)
+			})
+		})
+
+		Convey("When decoding the base58check Seed encoded form", func() {
+			actual, err := DecodeBase58Check(VersionByteSeed, seedEncoded)
+			So(err, ShouldBeNil)
+
+			Convey("The decoding should be correct", func() {
+				So(actual, ShouldResemble, unencoded)
+			})
+		})
+
+		Convey("When decoding as the wrong base58check encoded form", func() {
+			_, err := DecodeBase58Check(VersionByteAccountID, seedEncoded)
+
+			Convey("The returned error is correct", func() {
+				So(err, ShouldNotBeNil)
+				ivbErr, ok := err.(InvalidVersionByteError)
+				So(ok, ShouldBeTrue)
+				So(ivbErr.Actual, ShouldEqual, VersionByteSeed)
+				So(ivbErr.Expected, ShouldEqual, VersionByteAccountID)
 			})
 		})
 
