289432b4cee6c54a07a4dd3d6d1f2a3114815787
Scott Fleckenstein <nullstyle@gmail.com>
2016-03-02 09:31:38 -0800

    Add Address() helper to AccountId with simple tests

--- xdr/account_id.go
+++ xdr/account_id.go
@@ -2,6 +2,8 @@ package xdr
 
 import (
 	"errors"
+	"fmt"
+
 	"github.com/stellar/go-stellar-base/strkey"
 )
 
@@ -27,4 +29,22 @@ func (aid *AccountId) SetAddress(address string) error {
 	*aid, err = NewAccountId(CryptoKeyTypeKeyTypeEd25519, ui)
 
 	return err
+}
+
+// Address returns the strkey encoded form of this AccountId.  This method will
+// panic if the accountid is backed by a public key of an unknown type.
+func (aid *AccountId) Address() string {
+	if aid == nil {
+		return ""
+	}
+
+	switch aid.Type {
+	case CryptoKeyTypeKeyTypeEd25519:
+		ed := aid.MustEd25519()
+		raw := make([]byte, 32)
+		copy(raw, ed[:])
+		return strkey.MustEncode(strkey.VersionByteAccountID, raw)
+	default:
+		panic(fmt.Errorf("Unknown account id type: %v", aid.Type))
+	}
 }
